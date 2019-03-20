10f518d9269918da25d0176e765a89c3c54ac454
christopher <christoph.roa@gmail.com>
2019-03-20 18:29:51 -0500

    Add milestone.erb

--- routes.rb
+++ routes.rb
@@ -9,4 +9,19 @@ get '/user' do
   erb :user
 end
 
+###----christoph
+get '/milestone' do
+  erb :milestone
+end
+
+post "/save_weight_wanted" do
+  # store_weight_wanted(params["weight_w"])
+  puts params["weight_wanted"]
+  # "Ok!"
+  redirect "/user"
+end
+###--
+
 set :port, 8000
+
+
