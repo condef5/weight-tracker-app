c138815e8ff9f6dc182a6f36c920b82fb61f498a
Diego Torres <diegotc86@gmail.com>
2019-03-20 17:49:25 -0500

    Show user data on a table

--- routes.rb
+++ routes.rb
@@ -1,12 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader' # reload server
+require './models/User.rb'
 
 get '/' do
   erb :login
 end
 
-get '/user' do
-  erb :user
+# get '/user' do
+#   erb :user
+# end
+
+get "/view_measures" do
+  @user_data = User::find("diego@mail.com")
+  erb :view_measures
 end
 
 set :port, 8000
