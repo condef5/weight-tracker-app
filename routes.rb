579f45bc748fba607989d6da3262b938b2197da5
condef5 <davis.con.fab@gmail.com>
2019-03-21 10:04:15 -0500

    Add routes for registration users

--- routes.rb
+++ routes.rb
@@ -1,8 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader' # reload server
+require 'sinatra/flash'
+require './models/User'
+
+enable :sessions
 
 get '/' do
-  erb :login
+  if session[:user_email]
+    @current_user = User.find(session[:user_email]) 
+    erb :home
+  else
+    erb :register, :locals => { :hero => true }
+  end
+end
+
+post '/register' do
+  User::create(params)
+  session[:user_email] = params["email"]
+  flash[:message] = "Successful user registration"
+  redirect '/'
+end
+
+get '/login' do
+  erb :login, :locals => { :hero => true }
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
 
 get '/user' do
