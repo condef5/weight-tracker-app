b804d1ff82e8b8ebc0d49b95506293e44668db2c
Carlos A <cayala.w@gmail.com>
2019-03-20 20:37:55 -0500

    Week-4 Evaluation: Storage Measures, generateCSV.rb updated, requirement fulfilled.

--- controllers/generateCSV.rb
+++ controllers/generateCSV.rb
@@ -1,13 +1,30 @@
 require "csv"
 require "json"
 
+# name archive by date
 filename = Time.now.strftime("%Y%m%d")
-# puts filename
 
+# get json and parse it
 my_hashes = JSON.parse(File.open("./../data.json").read)
-# puts my_hashes
+# get all keys even teh ones in nested array
+cell_title = my_hashes.first.keys.first(4) + my_hashes.first["measures"].first.keys
 
+# open CSV file
 CSV.open("#{filename}.csv", "w") do |csv|
-  csv << my_hashes.first.keys
-  my_hashes.each { |hash| csv << hash.values }
+  # write completed column names
+  csv << cell_title
+  my_hashes.each do |hash|
+    hash["measures"].each do |mini_hash|
+      temp =[] # to clean container
+      temp << hash["email"]
+      temp << hash["name"]
+      temp << hash["gender"]
+      temp << hash["set_milestone"]
+      temp << mini_hash["date"]
+      temp << mini_hash["weight"]
+      temp << mini_hash["height"]
+      csv << temp # saving content to CSV
+    end
+  end
+
 end
