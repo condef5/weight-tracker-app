d2d7c98c7cf3d470687650be869a20140ddfff3e
condef5 <davis.con.fab@gmail.com>
2019-03-21 15:49:41 -0500

    Add comments for helpers.rb

--- helpers.rb
+++ helpers.rb
@@ -1,4 +1,7 @@
 helpers do
+  
+  # This method avoids having to recover the user in each route and
+  # makes it available in the instance variable current_user
   def current_user
     begin
       @current_user = User.find(session[:user_email])
@@ -7,10 +10,12 @@ helpers do
     end
   end
   
+  # This method protects the routes if the user is not logged in
   def protected!
     redirect '/login' if current_user.nil?
   end
 
+  # This method pass a message and status to show them to the user
   def set_flash(message, status = :success)
     type = {
       :success => "is-success", 
