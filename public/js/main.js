a87768a818a12cda69869c6b4a278674dc0c0756
condef5 <davis.con.fab@gmail.com>
2019-03-21 15:08:34 -0500

    Add function close for message flash

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,20 @@
-(function() {
-  var burger = document.querySelector('.burger');
-  var menu = document.querySelector('#' + burger.dataset.target);
-  burger.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', () => {
+  // Toogle menu hamburguer
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('#' + burger.dataset.target);
+  burger.addEventListener('click', () => {
     burger.classList.toggle('is-active');
     menu.classList.toggle('is-active');
   });
-})();
+
+  // Close message
+  const closeMessage = document.getElementById('close-message');
+  if (closeMessage) {
+    closeMessage.addEventListener('click', () => {
+      let message = document.getElementById('message');
+      if (message) {
+        message.style.display = 'none';
+      }
+    });
+  }
+});
