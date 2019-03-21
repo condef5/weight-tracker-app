29c1832112a25721bef94917a4383e4bddc9ffc8
Carlos A <cayala.w@gmail.com>
2019-03-21 09:59:09 -0500

    Week-4 Evaluation - Store Measures: Using main User class and cleaning code

--- controllers/generateCSV.rb
+++ controllers/generateCSV.rb
@@ -1,29 +1,36 @@
+# generateCSV uses data.json to generate the admin required CSV file
+
 require "csv"
 require "json"
+require_relative "../models/User"
 
 # name archive by date
 filename = Time.now.strftime("%Y%m%d")
 
 # get json and parse it
-my_hashes = JSON.parse(File.open("./../data.json").read)
-# get all keys even teh ones in nested array
-cell_title = my_hashes.first.keys.first(4) + my_hashes.first["measures"].first.keys
+# my_hashes = JSON.parse(File.open("data.json").read)
+
+# using class User to parse the Json file
+user = User.all
+
+# get all keys even the ones in nested array
+# cell_title = my_hashes.first.keys.first(4) + my_hashes.first["measures"].first.keys
+
+# setting column titles for CSV file
+cell_title = ["email", "name", "genre", "set_milestone", "date", "height", "weight"]
 
 # open CSV file
 CSV.open("#{filename}.csv", "w") do |csv|
   # write completed column names
   csv << cell_title
-  my_hashes.each do |hash|
-    hash["measures"].each do |mini_hash|
-      temp =[] # to clean container
-      temp << hash["email"]
-      temp << hash["name"]
-      temp << hash["gender"]
-      temp << hash["set_milestone"]
-      temp << mini_hash["date"]
-      temp << mini_hash["weight"]
-      temp << mini_hash["height"]
-      csv << temp # saving content to CSV
+# using main defined classes
+  user.each do |userdata|
+    userdata.measures.each do |each_measure|
+      temp = []
+      temp = [userdata.email, userdata.name, userdata.gender, userdata.set_milestone, each_measure.date, each_measure.height, each_measure.weight]
+
+# writing rows of data in CSV
+      csv << temp
     end
   end
 
