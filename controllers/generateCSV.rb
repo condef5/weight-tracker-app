77dda8a30d3a64639c2420d0095814a45e66b351
Carlos A <cayala.w@gmail.com>
2019-03-22 12:33:22 -0500

    Cleaning comments and unnecessary code from generateCSV

--- controllers/generateCSV.rb
+++ controllers/generateCSV.rb
@@ -8,20 +8,13 @@ def generateCSV
   # name archive by date
   filename = Time.now.strftime("%Y%m%d")
 
-  # get json and parse it
-  # my_hashes = JSON.parse(File.open("data.json").read)
-
   # using class User to parse the Json file
   users = User.all
-
-  # get all keys even the ones in nested array
-  # cell_title = my_hashes.first.keys.first(4) + my_hashes.first["measures"].first.keys
 
   # setting column titles for CSV file
   cell_title = ["email", "name", "genre", "set_milestone", "date", "height", "weight"]
 
   # open CSV file
-  # CSV.open("./public/#{filename}.csv", "w") do |csv|
   CSV.generate do |csv|
     # write completed column names
     csv << cell_title
@@ -44,4 +37,3 @@ def generateCSV
 
   end
 end
-# puts "Data CSV: \n#{generateCSV}"
